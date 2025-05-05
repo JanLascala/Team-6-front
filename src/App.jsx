ec7a23d49189225c387e7584d60f083b7b4fd04d
JanLascala <lascala.jan@gmail.com>
2025-05-05 17:56:41 +0200

    feat: add Home_page component and update App to include routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import DefaultLayout from "./Layouts/Default_layout";
+
+
 export default function App() {
   return (
+
     <DefaultLayout />
+
   )
 }
---
+++ src/Pages/Home_page.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Home_page() {
+    return (
+        <h1>this is home page</h1>
+    )
+}
