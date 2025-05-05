cf825c53574dde7c82340fd241a80428dc6ce133
JanLascala <lascala.jan@gmail.com>
2025-05-05 18:06:54 +0200

    feat: set up routing in App component with DefaultLayout and Home_page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import DefaultLayout from "./Layouts/Default_layout";
+import Home_page from './Pages/Home_page';
 
 
 export default function App() {
   return (
-
-    <DefaultLayout />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home_page />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
   )
 }
