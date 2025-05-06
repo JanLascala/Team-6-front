fe41b05610c9f777a130d5b12b04597979197d48
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-05-06 11:04:13 +0200

    feat: update Footer component structure and styling, enhance DefaultLayout with proper main styling, and add hover effect for footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function Footer() {
     return (
-        <h1>this is footer</h1>
+        <>
+            <footer className="footer py-4 bg-dark text-white text-center">
+                <p>© 2025 Il tuo negozio di vinili  <Link to="/about" className="text-white nav-link">About us</Link>  <Link to="/contact" className="text-white nav-link">Contatti</Link></p>
+            </footer>
+        </>
     )
 }
--- src/Layouts/Default_layout.jsx
+++ src/Layouts/Default_layout.jsx
@@ -7,7 +7,7 @@ export default function DefaultLayout() {
     return (
         <>
             <Header />
-            <main>
+            <main className="mb-5" style={{ minHeight: 'calc(100vh - 56px)' }}>
                 <Outlet />
             </main>
             <Footer />
