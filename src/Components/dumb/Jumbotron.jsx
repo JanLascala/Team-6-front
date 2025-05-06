9753071e11caa88344166f5aecbddf11846b0b8f
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-05-06 17:17:52 +0200

    add jumbotron to homepage + edit state handling

---
+++ src/Components/dumb/Jumbotron.jsx
@@ -0,0 +1,16 @@
+function Jumbotron() {
+    return (
+        <div className="container my-5">
+            <div className="p-5 mb-4 bg-light rounded-3 shadow-sm">
+                <div className="container-fluid py-5">
+                    <h1 className="display-5 fw-bold">Welcome to Our Store</h1>
+                    <p className="col-md-8 fs-4">
+                        Discover our curated collection of vinyl records, classics and new arrivals. Quality sound, timeless style.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,35 +1,33 @@
 import { useGlobalContext } from "../Contexts/GlobalContext";
+import Jumbotron from "../Components/dumb/Jumbotron";
 import Carousel from "../Components/dumb/carousel";
 import LoadingUi from "../Components/dumb/LoadingUi"
 import ServerErrorPage from "../Components/dumb/ServerErrorUi";
+
+
 export default function HomePage() {
     const { vinyls, isLoading } = useGlobalContext();
 
-    const renderList = () => {
-        switch (isLoading) {
+    switch (isLoading) {
 
-            case 'loading':
-                return <LoadingUi />
+        case 'loading':
+            return <LoadingUi />
 
-            case 'success':
+        case 'success':
 
-                return (
+            return (
+                <>
+                    <Jumbotron />
                     <Carousel array={vinyls} />
-                )
+                </>
+            )
 
-            case 'error':
-                return <ServerErrorPage error={"error"} />
+        case 'error':
+            return <ServerErrorPage error={"error"} />
 
-            default:
-                return <p>Unknown status</p>
+        default:
+            return <p>Unknown status</p>
 
-        }
     }
 
-    return (
-        <>
-            <h1 className="mb-4">Home page</h1>
-            {renderList()}
-        </>
-    )
 }
