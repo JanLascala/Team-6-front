8e1accae4c587442b34dd7b3388d27e3bc63add3
mattiapennisi <mattiapennisi0@gmail.com>
2025-05-07 15:58:24 +0200

    fix: remove duplicate import statement in SingleVinyl component

--- src/Pages/SingleVinyl.jsx
+++ src/Pages/SingleVinyl.jsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import { useParams } from "react-router-dom";
 import { useGlobalContext } from "../Contexts/GlobalContext";
 
 export default function VinylSearch() {
