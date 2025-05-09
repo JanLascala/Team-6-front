bbeda4b54b91db1c7f702089b1f3f3b6aaead533
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-05-09 10:16:14 +0200

    add carousel correlates by genre in single page

--- src/Pages/SingleVinyl.jsx
+++ src/Pages/SingleVinyl.jsx
@@ -1,33 +1,38 @@
+import { useGlobalContext } from "../Contexts/GlobalContext";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import LoadingUi from "../Components/LoadingUi";
 import ServerErrorPage from "./ServerErrorPage";
-import { useState, useEffect } from "react";
+import Carousel from "../Components/carousel";
 import AddToCartButton from "../Components/AddToCartButton";
+import filterByText from "../functions/filterByText";
 
 export default function VinylSearch() {
+    const { vinyls } = useGlobalContext();
+    console.log(vinyls)
     const { slug } = useParams();
-    const [vinyl, setVinyl] = useState({ state: 'loading' });
+    const [singleVinyl, setSingleVinyl] = useState({ state: 'loading' });
 
     useEffect(() => {
         fetch(`http://localhost:3000/api/vinyls/${slug}`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
-                setVinyl({ state: "success", vinyl_data: data });
+                //console.log(data);
+                setSingleVinyl({ state: "success", vinyl_data: data });
             })
             .catch((err) => {
-                console.error(err);
-                setVinyl({ state: 'error', message: `error type: ${err}` });
+                //console.error(err);
+                setSingleVinyl({ state: 'error', message: `error type: ${err}` });
             });
     }, [slug]);
 
-    switch (vinyl.state) {
+    switch (singleVinyl.state) {
 
         case 'loading':
             return <LoadingUi />;
 
         case 'success': {
-            const data = vinyl.vinyl_data;
+            const data = singleVinyl.vinyl_data;
 
             return (
                 <div className="container my-5">
@@ -96,12 +101,18 @@ export default function VinylSearch() {
                             ))}
                         </ol>
                     </div>
+
+                    <h1 style={{ paddingTop: "1rem" }}>We think you might like these!</h1>
+
+                    {/* did this to avoid sometimes vinyls not fetching fast enough and causing the page to crash as there was no array to map on */}
+                    {vinyls.state === "success" ? <Carousel array={filterByText(vinyls, data.genreName, "genre")} /> : <h6>Loading...</h6>}
+
                 </div>
             );
         }
 
         case 'error':
-            return <ServerErrorPage error={vinyl.message} />;
+            return <ServerErrorPage error={singleVinyl.message} />;
 
         default:
             return <p>Unknown status</p>;
